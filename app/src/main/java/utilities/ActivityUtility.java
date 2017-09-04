4d6c266ebfdd071a7f1c4345e1aac205109a916f
Jan Sarmiento <jxnsarmiento@gmail.com>
2017-09-04 12:44:43 +0800

    VCS Sample Commit

--- app/src/main/java/utilities/ActivityUtility.java
+++ app/src/main/java/utilities/ActivityUtility.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 
 /**
+ *
+ * Class to hold all boiler-plate codes
+ *
  * Created by JTS on 9/4/2017.
  */
-
 public class ActivityUtility {
 
     public ActivityUtility(){}
