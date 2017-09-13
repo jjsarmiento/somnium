f2e4065fd162a4e3d81110254aa679aae87f5ddc
Jan Sarmiento <jxnsarmiento@gmail.com>
2017-09-13 12:37:55 +0800

    ActionMode style

--- app/src/main/java/com/outmind/jts/somnium/BaseActivity.java
+++ app/src/main/java/com/outmind/jts/somnium/BaseActivity.java
@@ -1,15 +1,13 @@
 package com.outmind.jts.somnium;
 
-import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ActionMode;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.widget.Toast;
 
 import domain.BaseDomain;
@@ -18,11 +16,14 @@ import utilities.ActivityUtility;
 public class BaseActivity extends AppCompatActivity {
 
     ActivityUtility au;
+    ActionMode deleteActionMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+
+        this.setTitle("");
 
         BaseDomain.ML_CONMENU_ITEMS = getResources().getStringArray(R.array.conmen_array);
         BaseDomain.SL_CONMENU_ITEMS = getResources().getStringArray(R.array.conmen_sub_array);
@@ -34,6 +35,7 @@ public class BaseActivity extends AppCompatActivity {
 
         au = new ActivityUtility();
         au.refreshMasterList(this);
+
     }
 
     @Override
@@ -50,6 +52,8 @@ public class BaseActivity extends AppCompatActivity {
                 au.start(BaseActivity.this, AddListActivity.class);
                 break;
             case R.id.tm_delList :
+                deleteActionMode = this.startActionMode(deleteActionModeCallBack);
+                this.getWindow().getDecorView().findViewById(android.R.id.content).setSelected(true);
                 break;
             case R.id.tm_verBtn :
                 au.dialog("Somnium is developed by" + getResources().getString(R.string.app_author_name) + " \nApp version : " + getResources().getString(R.string.app_version_number) , this);
@@ -57,4 +61,30 @@ public class BaseActivity extends AppCompatActivity {
         }
         return true;
     }
+
+    private ActionMode.Callback deleteActionModeCallBack = new ActionMode.Callback() {
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            MenuInflater inflater = mode.getMenuInflater();
+            inflater.inflate(R.menu.delete_amode_menu, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            Toast.makeText(BaseActivity.this, item.getItemId(), Toast.LENGTH_SHORT).show();
+            mode.finish();
+            return false;
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            deleteActionMode = null;
+        }
+    };
 }
